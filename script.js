aaaea26b56ad13caf2b088d69618d0424ca3b938
elenchern <savkovaolga413@gmail.com>
2021-11-15 00:45:49 +0300

    plugin

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
+let player = videojs('video-exam', {
+     fluid: true,})
+const Component = videojs.getComponent('Component');
 
-var player = videojs('video-exam');
-//player.play();
-const chat=document.getElementById('chat')
-const input = document.querySelector("#inputMessage")
-const btnSend = document.querySelector("#btnSend")  
-const DMessage = document.querySelector("#message")  
-DMessage.innerHTML=localStorage.getItem('h');
-
-chat.style.visibility='hidden'
-function examplePlugin() {
-  player.on('playing', function() {//видимый чат
-    chat.style.visibility='visible'
-  });
-  btnSend.onclick  = function() {//прослушка на кнопке
-    var message = input.value; 
-    DMessage.innerHTML+= message + '<br/>'
-    localStorage.setItem('h', DMessage.innerHTML);
-    //localStorage.removeItem('ss');
+class ChatComponent extends Component{
+  constructor(player){
+    super(player)
   };
-  input.onkeyup = function(ent){
-    if(ent.keyCode==13){
-    var message = input.value; 
-    DMessage.innerHTML+= message + '<br/>'
-    localStorage.setItem('h', DMessage.innerHTML);}
-    else {null} 
+  createEl(){
+    const element = videojs.createEl('div', { 
+      className: 'vjs_chat',
+    });
+    element.insertAdjacentHTML('afterbegin', this.getTemplate())
+    return element;
+  };
+  getTemplate() {
+      return `
+      <div class="chat" id="chat">
+        <div class = "chat_message"><p id = "chat_message"></p></div>
+            <input id = "inputMessage" type = "text" placeholder = "Message...">
+            <div id="btnSend" class ="chat_btn"><button class="btnSend-btn">Send</div>
+          </div>`
   }
-}
+};
 
-examplePlugin();  
+Component.registerComponent('ChatComponent', ChatComponent);
 
-videojs.registerPlugin('examplePlugin', examplePlugin);
+function examplePlugin(){
+  let messageArray = [];
 
+  this.addChild('ChatComponent');
+  chat.style.visibility='hidden';
+  if ((JSON.parse(localStorage.getItem('key'))).length ==null){
+    id=0}
+  else{
+    id=(JSON.parse(localStorage.getItem('key'))).length;
+    for (var i = 0; i < (JSON.parse(localStorage.getItem('key'))).length; i++){
+      chat_message.innerHTML += (JSON.parse(localStorage.getItem('key')))[i].message +'<br/>';
+      messageArray = JSON.parse(localStorage.getItem('key'));
+    };
+  };
 
- 
+   this.on('playing', function(){
+     chat.style.visibility='visible';
+   });
+
+  btnSend.onclick  = function(){
+    messageArray.push({'id':id, 'message':inputMessage.value });
+    localStorage.setItem('key',  JSON.stringify(messageArray));
+    chat_message.innerHTML +=messageArray[id].message +'<br/>';
+    inputMessage.value='';
+    id+=1;
+  };
+
+  inputMessage.onkeyup = function(ent){
+    if(ent.keyCode==13){
+     messageArray.push({'id':id, 'message':inputMessage.value });
+      localStorage.setItem('key',  JSON.stringify(messageArray));
+     chat_message.innerHTML +=messageArray[id].message +'<br/>';
+     inputMessage.value='';
+      id+=1;
+    };
+  };
+  };
+
+ videojs.registerPlugin('examplePlugin', examplePlugin);
+ player.examplePlugin();
