9848e1c54835fb4c160b5d2c93b798e4e0641d8e
elenchern <savkovaolga413@gmail.com>
2021-11-16 22:23:39 +0300

    fix style code(2)

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-let player = videojs('video-exam', {
-  fluid: true,
-});
 const Component = videojs.getComponent('Component');
 let id = 0;
 
-class ChatComponent extends Component{
+class ChatComponent extends Component {
   constructor(player) {
-    super(player)
+    super(player);
   };
   createEl() {
     const element = videojs.createEl('div', {
       className: 'vjs-chat',
     });
-    element.insertAdjacentHTML('afterbegin', this.getTemplate())
+    element.insertAdjacentHTML( 'afterbegin', this.getTemplate() );
     return element;
   };
   getTemplate() {
     return `
-      <div class="vjs-chat_DivMessages"><p class="vjs-chat_pMessage"></p></div>
+      <div class="vjs-chat_messages-container"><p class="vjs-chat_pMessage"></p></div>
       <input class="vjs-chat_input" type="text" placeholder="Message...">
-      <div class ="vjs-chat_DivButton"><button class="vjs-chat_button">Send</div>`
+      <div class ="vjs-chat_button-container"><button class="vjs-chat_button">Send</div>`
   };
 };
 
@@ -31,26 +28,27 @@ function examplePlugin() {
   let vjs_chat = document.querySelector('.vjs-chat');
   let chat_message = document.querySelector('.vjs-chat_pMessage');
   let inputMessage = document.querySelector('.vjs-chat_input');
-  let btnSend = document.querySelector('.vjs-chat_DivButton');
-  
+  let btnSend = document.querySelector('.vjs-chat_button-container');
+  let saveMessage = JSON.parse( localStorage.getItem('messageList') );
+
   vjs_chat.style.visibility = 'hidden';
-  if ( ( JSON.parse( localStorage.getItem('key') ) ) == null ) {
+  if (saveMessage == null) {
     id = 0;
   } else {
-    id = ( JSON.parse( localStorage.getItem('key') ) ).length;
-    for (var i = 0; i < ( JSON.parse( localStorage.getItem('key') ) ).length; i++) {
-      chat_message.innerHTML += ( JSON.parse( localStorage.getItem('key') ) )[i].message + '<br/>';
-      messageArray = JSON.parse( localStorage.getItem('key') );
+    id = saveMessage.length;
+    for (var i = 0; i < saveMessage.length; i++) {
+      chat_message.innerHTML += saveMessage[i].message + '<br/>';
+      messageArray = saveMessage;
     };
   };
-  
+
    this.on('playing', function() {
     vjs_chat.style.visibility = 'visible';
    });
 
   btnSend.onclick = function() {
     messageArray.push({'id':id, 'message':inputMessage.value});
-    localStorage.setItem( 'key', JSON.stringify(messageArray) );
+    localStorage.setItem( 'messageList', JSON.stringify(messageArray) );
     chat_message.innerHTML += messageArray[id].message + '<br/>';
     inputMessage.value = '';
     id += 1;
@@ -59,7 +57,7 @@ function examplePlugin() {
   inputMessage.onkeyup = function(ent) {
     if(ent.keyCode==13) {
       messageArray.push({'id':id, 'message':inputMessage.value});
-      localStorage.setItem( 'key', JSON.stringify(messageArray) );
+      localStorage.setItem( 'messageList', JSON.stringify(messageArray) );
       chat_message.innerHTML += messageArray[id].message + '<br/>';
       inputMessage.value = '';
       id += 1;
@@ -68,5 +66,11 @@ function examplePlugin() {
 };
 
 videojs.registerPlugin('examplePlugin', examplePlugin);
-player.examplePlugin();
+
+videojs('video-exam', {
+  fluid: true,
+  plugins: {
+      examplePlugin: true
+  }
+});
 
