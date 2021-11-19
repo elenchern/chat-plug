a3dcaf815cfc9bf5ab9a4d833ac361fcf88d87fa
elenchern <savkovaolga413@gmail.com>
2021-11-19 16:08:24 +0300

    fix style code(5)

--- script.js
+++ script.js
@@ -16,40 +16,41 @@ class ChatComponent extends Component {
     return `
       <div class="vjs-chat__messages-container"><p class="vjs-chat__message-p"></p></div>
       <input class="vjs-chat__input" type="text" placeholder="Message...">
-      <div class ="vjs-chat__button-container"><button class="vjs-chat__button">Send</div>`
+      <div class ="vjs-chat__button-container"><button class="vjs-chat__button">Send</div>`;
   };
 };
 
 Component.registerComponent('ChatComponent', ChatComponent);
 
-function examplePlugin() {
+function ExamplePlugin() {
   this.addChild('ChatComponent');
   let messageArray = [];
-  let vjs_chat = document.querySelector('.vjs-chat');
-  let chat_message = document.querySelector('.vjs-chat__message-p');
+  let vjsChat = document.querySelector('.vjs-chat');
+  let chatMessage = document.querySelector('.vjs-chat__message-p');
   let inputMessage = document.querySelector('.vjs-chat__input');
   let btnSend = document.querySelector('.vjs-chat__button-container');
   let saveMessage = JSON.parse( localStorage.getItem('messageList') );
 
-  vjs_chat.style.visibility = 'hidden';
+  vjsChat.style.visibility = 'hidden';
+
   if (saveMessage == null) {
     id = 0;
   } else {
     id = saveMessage.length;
     for (var i = 0; i < saveMessage.length; i++) {
-      chat_message.innerHTML += saveMessage[i].message + '<br/>';
+      chatMessage.innerHTML += saveMessage[i].message + '<br/>';
       messageArray = saveMessage;
     };
   };
 
-   this.on('playing', function() {
-    vjs_chat.style.visibility = 'visible';
-   });
+  this.on('playing', function() {
+    vjsChat.style.visibility = 'visible';
+  });
 
   btnSend.onclick = function() {
     messageArray.push({'id':id, 'message':inputMessage.value});
     localStorage.setItem( 'messageList', JSON.stringify(messageArray) );
-    chat_message.innerHTML += messageArray[id].message + '<br/>';
+    chatMessage.innerHTML += messageArray[id].message + '<br/>';
     inputMessage.value = '';
     id += 1;
   };
@@ -58,19 +59,19 @@ function examplePlugin() {
     if(ent.keyCode==13) {
       messageArray.push({'id':id, 'message':inputMessage.value});
       localStorage.setItem( 'messageList', JSON.stringify(messageArray) );
-      chat_message.innerHTML += messageArray[id].message + '<br/>';
+      chatMessage.innerHTML += messageArray[id].message + '<br/>';
       inputMessage.value = '';
       id += 1;
     };
   };
 };
 
-videojs.registerPlugin('examplePlugin', examplePlugin);
+videojs.registerPlugin('ExamplePlugin', ExamplePlugin);
 
 videojs('video-exam', {
   fluid: true,
   plugins: {
-      examplePlugin: true
+    ExamplePlugin: true
   }
 });
 
